b3c3d94325c8ac20aca76f66804b042f07979539
魏名昕 <weimingxin@gridsum.com>
2018-03-14 19:34:32 +0800

    1

--- app/router.js
+++ app/router.js
@@ -6,4 +6,7 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
+  /* login */
+  router.post('/api/login', controller.login.index);
+  
 };
