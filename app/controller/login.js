14428bed95ced4e72391a80af639b587822a344b
wmx <1043426948@qq.com>
2018-03-14 23:21:49 +0800

    1

--- app/controller/login.js
+++ app/controller/login.js
@@ -3,9 +3,15 @@
 const Controller = require('egg').Controller;
 
 class LoginController extends Controller {
+  // get
   async index() {
     const { ctx } = this;
-    ctx.body = `body: ${JSON.stringify(ctx.request.body)}`;
+    ctx.body = `body: ${JSON.stringify(ctx.query.id)}`;
+  }
+  // login
+  async create() {
+    const { ctx, service } = this;
+    ctx.body = ctx.request.body;
   }
 }
 
--- app/router.js
+++ app/router.js
@@ -7,6 +7,6 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
   /* login */
-  router.post('/api/login', controller.login.index);
+  router.resources('login', '/api/login', controller.login);
   
 };
